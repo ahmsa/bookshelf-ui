ff2007bccafd50f0c7fa034db0d0ba4fda338eca
Mouhanad Al-Ramli <mouhanad.al-ramli@gm.com>
2025-10-21 16:24:42 -0400

    Continuing impl

---
+++ src/app/genres/genre-card/genre-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-card',
+  imports: [],
+  templateUrl: './genre-card.html',
+  styleUrl: './genre-card.css'
+})
+export class GenreCard {
+
+}
