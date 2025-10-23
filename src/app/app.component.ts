f77acd8f40ff4469026437f669b5cb9cd9431f28
Mouhanad Al-Ramli <mouhanad.alramli@gmail.com>
2025-10-22 22:40:06 -0400

    Add Banner and Main Menu components with styling and tests; update Angular Material theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BannerComponent } from './banner/banner/banner.component';
+import { BannerComponent } from './banner/banner.component';
+import { MainMenuComponent } from "./banner/main-menu-component/main-menu-component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [BannerComponent, RouterOutlet]
+    imports: [BannerComponent, RouterOutlet, MainMenuComponent]
 })
 export class AppComponent {
   title = 'bookshelf-ui';
---
+++ src/app/banner/main-menu-component/main-menu-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-main-menu-component',
+  imports: [MatMenuModule],
+  templateUrl: './main-menu-component.html',
+  styleUrl: './main-menu-component.css'
+})
+export class MainMenuComponent {
+
+}
