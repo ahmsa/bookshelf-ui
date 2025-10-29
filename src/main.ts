5f3a5a2f4c56ef07fd06ea6619ef93a39adc0bb3
Mouhanad Al-Ramli <mouhanad.al-ramli@gm.com>
2025-10-29 15:40:53 -0400

    Moving from app routing module to app routes

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { importProvidersFrom } from '@angular/core';
 import { App } from './app/app';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { providePrimeNG } from 'primeng/config';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { routes } from "./app/app.routes";
 
 
 bootstrapApplication(App, {
     providers: [
-      importProvidersFrom(BrowserModule, AppRoutingModule),
+      importProvidersFrom(BrowserModule),
       providePrimeNG(),
       provideHttpClient(),
-      provideAnimations()
+      provideAnimations(),
+      provideRouter(routes, withComponentInputBinding())
+
     ]
 })
   .catch(err => console.error(err));
