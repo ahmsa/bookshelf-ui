9599e9c1bf225f91de3a81e738f25b6459995cfd
Mouhanad Al-Ramli <ahmsa@users.noreply.github.com>
2025-11-08 17:29:10 -0500

    Feat/ma build bookshelf (#4)
    * Refactor app layout to nest main menu component within the banner; add spacing in banner component
    
    * Refactor main menu component and banner layout; add styles for table and menu
    
    * Modernizing components
    
    * Refactor routing and author components; add caching to AuthorService and improve UI interactions
    
    * Refactor author handling: update ID field to be nullable, enhance author service caching, and improve author list navigation
    
    * add delete button for author
    
    * Remove console log entries and move styles to main styles.css
    
    * Working on Genres
    
    * Working on Genres
    
    * Working on Genres
    
    * Working on Genres
    
    * Working on Genres
    
    * Moving from app routing module to app routes
    
    * Reading route param as input
    
    * Fixed bug with genre service
    
    * Fixed bug with genre service and author service
    
    * UI work
    
    * Forms styles
    
    * Using name of parent genre
    
    * Adding sub-genre length and disable delete when sub-genres exist.
    
    * Adding books logic
    
    * Add publication year to books
    
    * Create author dorpdown in book details

--- src/app/books/book-details/book-details.ts
+++ src/app/books/book-details/book-details.ts
@@ -1,12 +1,21 @@
+import { GenreService } from './../../genres/genre-service';
 import { Component, inject, Input, output, Output } from '@angular/core';
 import { Book } from "../../../data/book";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BooksService } from "../books-service";
+import { AuthorService } from 'src/app/authors/author-service';
+import { AsyncPipe } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-book-details',
   imports: [
-    FormsModule
+    FormsModule,
+    AsyncPipe,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   templateUrl: './book-details.html',
   styleUrl: './book-details.css'
@@ -14,9 +23,15 @@ import { BooksService } from "../books-service";
 export class BookDetails {
     @Input({ required: true }) book: Book = new Book();
 
+    authorService: AuthorService = inject(AuthorService);
+    genreService: GenreService = inject(GenreService);
+
     close = output<boolean>();
 
     private readonly booksService: BooksService = inject(BooksService);
+
+    allAuthors$ = this.authorService.getAllAuthors();
+    allGenres$ = this.genreService.getAllGenres();
 
   protected onSubmit(book: Book) {
     this.booksService.saveBook(book).subscribe(() => {
